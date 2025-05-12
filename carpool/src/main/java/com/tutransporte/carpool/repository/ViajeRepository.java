b6cee9f9e600fc64fe2f4c1d3683a59fd987c378
DanielGranadosS <Daniel-Granados1@Eep-Igroup.Com>
2025-05-12 12:39:56 +0200

    avance back

---
+++ carpool/src/main/java/com/tutransporte/carpool/repository/ViajeRepository.java
@@ -0,0 +1,8 @@
+package com.tutransporte.carpool.repository;
+
+import com.tutransporte.carpool.model.Viaje;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ViajeRepository extends JpaRepository<Viaje, Long> {
+
+}
